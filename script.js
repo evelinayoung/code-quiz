0a14c41d0804d6a9d88b882270a08d337df7f6cf
Niyoki <evelinaniyoki@gmail.com>
2021-10-08 18:28:43 -0400

    setting up questions to be hidden

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+
 // Timer Setup 
 
 var timer;
 var timeLeft=0;
+var start= startTimer();
 
 function startTimer() {
     timeLeft= 100;
     timer= setInterval(function(){
         timeLeft--;
+        if(timeLeft===0){
+            finish();
+        }
         document.getElementById("timer").innerText=timeLeft ;
     },1000)
 }
+
+function finish(){
+    clearInterval(timer);
+    alert("Game Over!")
+}
+
+function subtractTime(){
+    timeLeft -=10;
+    if(timeLeft===0){
+        finish();    }
+}
 startTimer();
+
+// Question Button Setup 
+
+function startQuiz(){
+    $("#start").hide;
+}
+
+(document).ready(function(){
+    $("#start").on('click', function()
+}
